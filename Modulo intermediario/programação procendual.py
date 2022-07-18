a02c2ba40717373cbc1eab357729ac3d42ad9163
João José <106547804+Johnzmm@users.noreply.github.com>
2022-07-18 13:33:23 -0300

    Update programação procendual.py

--- Modulo intermediario/programação procendual.py
+++ Modulo intermediario/programação procendual.py
@@ -1,11 +1,51 @@
-"""
-Funções do python
-def função parte 01
-"""
+# exercicio 01
 
-def funcao():
-    print('lindas')
-funcao()
-funcao()
-funcao()
-funcao()
+# crie uma funcao que exiba uma saudaçao com os parametros saudaçao e nome
+
+
+def saudacao(msg='Olá', nome='João'):
+    msg = msg
+    nome = nome
+    return f'{msg} me chamo {nome}'
+
+
+funcao = saudacao()
+print(funcao)
+
+
+# exercicio 02
+# faça uma funcao que receba 3 numeros com parametros e exiba a soma entre eles
+
+def soma(n1=10, n2=20):
+    n1 = n1
+    m2 = n2
+    return n1 + n2
+
+
+_soma = soma()
+print(_soma)
+
+
+# exercicio 03
+# crie uma funcao que receba 2 numeros, o primeiro é um valor e o segundo um percentual retorne (return) o primeiro valor
+# do primeiro numero somado do aumento do percentual do mesmo
+
+def soma_percentual(valor, percentual):
+    return valor + (valor * percentual / 100)
+
+soma_de_valores = soma_percentual(50,10)
+print(soma_de_valores)
+
+# Exercicio 04
+# Fizz Buzz
+
+def fb(numero):
+    if numero % 3 == 0 and numero % 5 == 0:
+        return 'FizzBuzz'
+    if numero % 5 == 0:
+        return 'Buzz'
+    if numero % 3 == 0:
+        return 'Fizz'
+    return numero
+
+print(fb(15))
